a191f289f3efd129cf98b9099e3d06ce920eba3d
gxs <lyricsong47@gmail.com>
2025-01-12 21:43:49 +0800

    chore(ci): fix codecov reports

---
+++ Elasticsearch.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace Cicada\Elasticsearch;
+
+use Cicada\Core\Framework\Bundle;
+use Cicada\Core\Framework\Log\Package;
+use Cicada\Elasticsearch\DependencyInjection\ElasticsearchExtension;
+use Cicada\Elasticsearch\DependencyInjection\ElasticsearchMigrationCompilerPass;
+use Cicada\Elasticsearch\Profiler\ElasticsearchProfileCompilerPass;
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
+
+/**
+ * @internal
+ */
+#[Package('core')]
+class Elasticsearch extends Bundle
+{
+    public function getTemplatePriority(): int
+    {
+        return -1;
+    }
+
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+        $this->buildDefaultConfig($container);
+
+        $container->addCompilerPass(new ElasticsearchMigrationCompilerPass());
+
+        // Needs to run before the ProfilerPass
+        $container->addCompilerPass(new ElasticsearchProfileCompilerPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 5000);
+    }
+
+    protected function createContainerExtension(): ?ExtensionInterface
+    {
+        return new ElasticsearchExtension();
+    }
+}
---
+++ Framework/Indexing/ElasticsearchIndexingMessage.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Cicada\Elasticsearch\Framework\Indexing;
+
+use Cicada\Core\Framework\Context;
+use Cicada\Core\Framework\Log\Package;
+use Cicada\Core\Framework\MessageQueue\AsyncMessageInterface;
+
+#[Package('core')]
+class ElasticsearchIndexingMessage implements AsyncMessageInterface
+{
+    /**
+     * @internal
+     */
+    public function __construct(
+        private readonly IndexingDto $data,
+        private readonly ?IndexerOffset $offset,
+        private readonly Context $context
+    ) {
+    }
+
+    public function getData(): IndexingDto
+    {
+        return $this->data;
+    }
+
+    public function getOffset(): ?IndexerOffset
+    {
+        return $this->offset;
+    }
+
+    public function getContext(): Context
+    {
+        return $this->context;
+    }
+}
